2a1bd99ba7c3d1c86c047d2d10049e6c1fcf1c2e
jharold333 <jharold.morenos@udea.edu.co>
2023-04-10 22:31:33 -0500

    se agrega decodificador

--- lab3banco/main.cpp
+++ lab3banco/main.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <conio.h>
 #include <windows.h>
+#include <bitset>
 
 using namespace std;
 
@@ -28,6 +29,21 @@ using namespace std;
    string forma3(string , string , int , int);
    void escribir(string nombre, string contenido);
    string string2Bin(string nombre);
+   //decodificar
+   string bloque1(string binario, unsigned long long semilla);
+   string Invertir(string bloq1);
+   int Numero_de_Regla(string bloque1);
+   string bloques_consecutivos (string binary, unsigned long long semilla, unsigned long long contador);
+   string Aplicar_Regla(string bloque, int regla);
+   void Generar_Archivo_Decodificado(string archivo);
+   string leer_archivo(string Rut);
+   string metodo_decodificacion_2(string bloque, unsigned long long semilla);
+   int De_Binario_Decimal (string bloque_N);
+   string metodo_codificacion2(string bloque, unsigned long long semilla);
+
+
+
+
 
    // codificacion y descoidificacion de 2 metodos
 
@@ -42,7 +58,8 @@ int main(){
 
     texto1 = leer("Sudo.txt");
 
-    cout<<"la clave es "<<texto1<<endl;
+    cout<<"la clave es abcd para decodificar ingresa el texto codificado "<<texto1<<endl;
+    //decodifica mejor por el 2 y el texto tiene que estar en el .txt
 
     cout << "Ingrese clave del administrador: " <<endl;
 
@@ -60,7 +77,7 @@ int main(){
 
     mainMenu();
 
-    while (k!=5){
+    while (k!=6){
         cout<<"ingresa una opcion: "<<endl;
         cin>>k;
         cin.clear();
@@ -122,9 +139,169 @@ int main(){
     }
 
     else if (k==4){
+        string Ruta, Nombre, archivo, binary, archivo_primer_metodo, bloque, guardar_bloque, archivo_segundo_metodo;
+            unsigned long long semilla, longitud, contador, repos;
+            int regla, metodo;
+        cout<<endl;
+        cout<<endl;
+        string respuesta;
+        cout<<" _______________________________________________"<<endl;
+        cout<<"|    Desea Decodificar el Archivo  [Y] o [N]  | "<<endl;
+        cout<<" _______________________________________________"<<endl;
+        cout<<endl;
+        cout<<"Ingrese su Respuesta: ";
+        cin>>respuesta;
+        cout<<endl;
+
+        if(respuesta== "Y"){
+            cout<<"Ingrese el Metodo de Codificacion Usado [1] o [2]: ";
+            cin>>metodo;
+            cout<<endl;
+
+            if(metodo==1){
+                //    cout<<endl;
+                //    cout<<"Ingresar [Direccion] del Archivo: ";
+                //    cin>>Ruta;
+                //    cout<<endl;
+                //    cout<<"Ingresar [Nombre] del Archivo: ";
+                //    cin>>Nombre;
+                //    Nombre=Nombre+".txt";
+                //    Ruta=Ruta+Nombre;
+                //archivo_primer_metodo.erase();
+                Ruta="Sudo.txt";
+                archivo=archivo_primer_metodo;
+                cout<<"Ingrese La Semilla de Codificacion: ";
+                cin>>semilla;
+                cout<<endl;
+                repos=semilla;
+                longitud=archivo.length();
+                if(semilla>=longitud){
+                    for(unsigned long long i=0; i<longitud;i++){
+                        if(archivo[i]=='1'){
+                            archivo_primer_metodo=archivo_primer_metodo+"0";
+                        }
+                        else{
+                            archivo_primer_metodo=archivo_primer_metodo+"1";
+                        }
+                    }
+                }
+                else{
+                    bloque=bloque1(archivo, semilla);
+                    archivo_primer_metodo=Invertir(bloque);
+                    regla=Numero_de_Regla(archivo_primer_metodo);
+                    contador= semilla;
+                    semilla=semilla+repos;
+                    binary=archivo;
+                    while(1>0){
+                        bloque.erase();
+                        guardar_bloque.erase();
+                        //Reorganizar:
+                        bloque=bloques_consecutivos(binary,semilla,contador);
+                        guardar_bloque=Aplicar_Regla(bloque, regla);
+                        regla=Numero_de_Regla(guardar_bloque);
+                        archivo_primer_metodo=archivo_primer_metodo+ guardar_bloque;
+
+                        contador= semilla;
+                        semilla=semilla+repos;
+
+                        if(contador==binary.length()){
+                            cout<<endl;
+                            cout<<archivo_primer_metodo<<endl;
+                            cout<<endl;
+                            cout<<"Longitud= "<<archivo_primer_metodo.length()<<endl;
+                            //generar_archivo_codificado(archivo_primer_metodo);
+                            cout<<"  revisar el Sudo.txt  "<<endl;
+                            cout<<"  [----Archivo Decodificado----]  "<<endl;
+                            Generar_Archivo_Decodificado(archivo_primer_metodo);
+                            break;
+                        }
+
+                        if(semilla>binary.length()){
+                            bloque.erase();
+                            guardar_bloque.erase();
+                            while(contador!=(binary.length())){
+                                bloque=bloque+binary[contador];
+                                contador=contador+1;
+                            }
+                            guardar_bloque=Aplicar_Regla(bloque, bloque.length());
+                            archivo_primer_metodo=archivo_primer_metodo+guardar_bloque;
+                            cout<<archivo_primer_metodo<<endl;
+                            cout<<endl;
+                            //cout<<"Longitu ="<<archivo_primer_metodo.length()<<endl;
+                            //generar_archivo_codificado(archivo_primer_metodo);
+                            cout<<"  [ [----Archivo Decodificado----]]  "<<endl;
+                            Generar_Archivo_Decodificado(archivo_primer_metodo);
+                            break;
+                        }
+
+                    }
+
+                }
+
+            }
+            if(metodo==2){
+                archivo_segundo_metodo.erase();
+                Ruta="Sudo.txt";
+                archivo=leer_archivo(Ruta);
+                cout<<"Ingrese La Semilla de Codificacion: ";
+                cin>>semilla;
+                cout<<endl;
+                repos=semilla;
+                longitud=archivo.length();
+                binary=archivo;
+                contador=0;
+                while(1>0){
+                    bloque=bloques_consecutivos(binary,semilla,contador);
+                    guardar_bloque=metodo_decodificacion_2(bloque, repos);
+                    archivo_segundo_metodo=archivo_segundo_metodo+guardar_bloque;
+                    contador= semilla;
+                    semilla=semilla+repos;
+
+                    if(contador==binary.length()){
+                        cout<<endl;
+                        cout<<archivo_segundo_metodo<<endl;
+                        cout<<endl;
+                        //cout<<"Longitud= "<<archivo_primer_metodo.length()<<endl;
+                        //generar_archivo_codificado(archivo_primer_metodo);
+                        cout<<"  [----Archivo Decodificado----]  "<<endl;
+                        Generar_Archivo_Decodificado(archivo_segundo_metodo);
+                        break;
+                    }
+
+                    if(semilla>binary.length()){
+                        bloque.erase();
+                        guardar_bloque.erase();
+                        while(contador!=(binary.length())){
+                            bloque=bloque+binary[contador];
+                            contador=contador+1;
+                        }
+                        guardar_bloque=metodo_decodificacion_2(bloque, bloque.length());
+                        archivo_segundo_metodo=archivo_segundo_metodo+guardar_bloque;
+                        cout<<archivo_segundo_metodo<<endl;
+                        cout<<endl;
+                        //cout<<"Longitu ="<<archivo_primer_metodo.length()<<endl;
+                        //generar_archivo_codificado(archivo_primer_metodo);
+                        cout<<"  [ [----Archivo Decodificado----]]  "<<endl;
+                        Generar_Archivo_Decodificado(archivo_segundo_metodo);
+                        break;
+
+            }
+
+
+        }
+
+
+
+     }
+   }
+ }
+
+    else if (k==5){
+
 
         cout<<"Fin del Programa "<<endl;
         break;
+
     }
 
   }
@@ -147,7 +324,8 @@ void mainMenu(){
            "           ||	1-> Registrar usuarios                  ||\n"
            "           ||	2-> Buscar un usuario                   ||\n"
            "           ||	3-> encriptar archivo                   ||\n"
-           "           ||	4-> Cerrar programa                     ||\n"
+           "           ||	4-> desencriptar archivo                ||\n"
+           "           ||	5-> Cerrar programa                     ||\n"
            "           ||                                           ||\n"
            "          ===============================================\n"<<endl;
 }
@@ -384,4 +562,239 @@ string string2Bin(string nombre){
         textoBin+=letraBin;
     }
     return textoBin;
+}
+
+
+string bloque1(string binario, unsigned long long semilla){
+    string bloque1;
+    for(unsigned long long i=0; i!=semilla;i++){
+        bloque1=bloque1+binario[i];
+    }
+
+    return bloque1;
+}
+
+string Invertir(string bloq1){
+    string invertido;
+    for(unsigned long long i=0; i<bloq1.length();i++){
+        if(bloq1[i]=='1'){
+            invertido=invertido+"0";
+        }
+        else{
+            invertido=invertido+"1";
+        }
+    }
+    return invertido;
+}
+
+int Numero_de_Regla(string bloque1){//Cuenta los 1s y los 0s
+    unsigned long long contador_0s=0, contador_1s=0;
+    int hg=0;
+    for(unsigned long long i=0; i!=bloque1.length();i++){
+        if (bloque1[i]=='1'){
+            contador_1s=contador_1s+1;
+        }
+        else{
+            contador_0s=contador_0s+1;
+        }
+    }
+
+    if (contador_0s==contador_1s){
+
+        hg=hg+1;
+    }
+
+    if (contador_0s>contador_1s){
+        hg=hg+2;
+    }
+    if(contador_1s>contador_0s){
+        hg=hg+3;
+    }
+
+    return hg;
+}
+
+string bloques_consecutivos (string binary, unsigned long long semilla, unsigned long long contador){
+    string bloque_siguiente;
+    while (contador!=semilla){
+        bloque_siguiente=bloque_siguiente+binary[contador];
+        contador=contador+1;
+    }
+    return bloque_siguiente;
+}
+
+
+
+
+string Aplicar_Regla(string bloque, int regla){
+    unsigned long long contador=0, col=1;
+    string retorno;
+
+    if (regla ==1){
+        for(unsigned long long i=0; i<bloque.length(); i++){
+            if (bloque[i]=='1'){
+                retorno=retorno+'0';
+            }
+            else{
+                retorno=retorno+'1';
+            }
+        }
+    }
+
+    if (regla==2){
+        while(contador<bloque.length()){
+            if(col==2){
+                if (bloque[contador]=='1'){
+                    retorno=retorno+'0';
+                }
+                else{
+                    retorno=retorno+'1';
+                }
+                col=1;
+            }
+            else{
+                retorno=retorno+bloque[contador];
+                col=col+1;
+            }
+            contador=contador+1;
+
+        }
+    }
+
+    if (regla==3){
+        while(contador<bloque.length()){
+            if(col==3){
+                if (bloque[contador]=='1'){
+                    retorno=retorno+'0';
+                }
+                else{
+                    retorno=retorno+'1';
+                }
+                col=1;
+            }
+            else{
+                retorno=retorno+bloque[contador];
+            }
+            contador=contador+1;
+            col=col+1;
+        }
+    }
+
+    return retorno;
+}
+
+void Generar_Archivo_Decodificado(string archivo){
+    unsigned semilla=8, contador=0, repos, longitud;
+    int numero;
+    string guardar, generador;
+    repos=semilla;
+    longitud=archivo.length();
+    while(1>0){
+        guardar=bloques_consecutivos(archivo, semilla, contador);
+        numero=De_Binario_Decimal(guardar);
+        contador= semilla;
+        semilla=semilla+repos;
+        char a=numero;
+        generador=generador+a;
+        if(contador==longitud){
+            break;
+        }
+        if(semilla>longitud){
+            break;
+        }
+    }
+
+    ofstream archivo2;
+
+    archivo2.open("Sudo.txt",ios::out);
+    if(archivo2.fail()){
+        cout<<"No se puedo Generar el Archivo Codificado"<<endl;
+        exit(1);
+    }
+    archivo2<<generador;
+
+    archivo2.close();
+
+
+
+
+}
+
+
+string leer_archivo(string Rut){
+        char B;
+        string archivo;
+        fstream text;
+        unsigned long long tama;
+        text.open(Rut, ios_base::in | ios_base::binary| ios_base:: ate);
+        //Si falla al Abrirn el Archivo:
+        if (!text.is_open()){
+            archivo=" ";
+        }
+        else{
+            tama=text.tellg();
+            text.seekg(0);
+            for(unsigned long long i=0; i<tama; i++ ){
+                text.get(B);
+                archivo+=B;
+            }
+        }
+        text.close();
+
+        return archivo;
+    }
+
+string metodo_decodificacion_2(string bloque, unsigned long long semilla){
+    string retorno;
+    unsigned long long contador=1;
+    while (contador!=semilla){
+        if (contador==(semilla-1)){
+            retorno=retorno+bloque[contador];
+            contador=contador+1;
+        }
+        else{
+            retorno=retorno+bloque[contador];
+            contador=contador+1;
+        }
+    }
+    retorno=retorno+bloque[0];
+
+    return retorno;
+
+}
+
+int De_Binario_Decimal (string bloque_N){
+    int resto=0, resultado;
+    int digito[8];
+    int binario = std::atoi(bloque_N.c_str());
+    for(int i=0; i<8; i++){
+        digito[i]=binario%10;
+        binario /=10;
+    }
+
+    for(int i=7; i>=0; i--){
+        resultado= (resto*2)+digito[i];
+        resto=resultado;
+    }
+
+    return resultado;
+}
+string metodo_codificacion2(string bloque, unsigned long long semilla){
+    string retorno;
+    unsigned long long contador=0;
+    retorno=retorno+bloque[semilla-1];
+    while (contador!=semilla){
+        if (contador==(semilla-1)){
+            contador=contador+1;
+        }
+        else{
+            retorno=retorno+bloque[contador];
+            contador=contador+1;
+        }
+
+
+    }
+    return retorno;
+
+
 }
